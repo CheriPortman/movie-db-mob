8b2da9bf0aaf2c2d9a4750ae91485010f14891db
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-13 11:35:23 -0700

    finished for the day

--- src/favorites.js
+++ src/favorites.js
@@ -1,6 +1,7 @@
 import loadHeader from './header-component.js';
 import { auth, favoritesByUserRef } from './firebase.js';
-
+import convertObjToArray from './convert-obj-to-array.js';
+import loadMovieList from './list-component.js';
 loadHeader();
 
 auth.onAuthStateChanged(user => {
@@ -8,5 +9,7 @@ auth.onAuthStateChanged(user => {
     const userFavoritesRef = favoritesByUserRef.child(userID);
     userFavoritesRef.on('value', snapshot => {
         const value = snapshot.val();
+        const favoriteMovies = convertObjToArray(value);
+        loadMovieList(favoriteMovies);
     });
 });
